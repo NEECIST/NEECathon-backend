d9ce75a5a4ba82cf7b968cff58afe760bac615d6
Varsenaiga <tiago.diabolico@gmail.com>
2021-10-01 16:33:40 +0100

    Added Pot endpoints

--- endpoints.js
+++ endpoints.js
@@ -1,9 +1,11 @@
 import { supabase } from './settings.js'
 import * as functions from './functions.js'
 
+var potID = 0;
+
 async function addCoins(teamID,cash){
   if(teamID < 0 || cash<0){
-    return;
+    return false;
   }
   let { data: Teams, error } = await supabase
   .from('Teams')
@@ -23,18 +25,19 @@ async function addCoins(teamID,cash){
         //NOTE equipa não encontrada
     }
   }
+  return true
 }
 
 async function subtractCoins(teamID,cash){
   if(teamID < 0 || cash<0){
-    return;
+    return false;
   }
   let { data: Teams, error } = await supabase
   .from('Teams')
   .select('*').eq('IDTEAM', teamID)
 
   if(Teams.length && error==null){
-    Teams[0].CASH=(Teams[0].CASH-=cash) <0 ? 0: Teams[0].CASH;
+    Teams[0].CASH=(Teams[0].CASH-=cash) <0 ? 0: Teams[0].CASH;  //REVIEW impedir ação se não tiver dinheiro suficiente
     const { updated, update_error } = await supabase
     .from('Teams')
     .update({ CASH: Teams[0].CASH })
@@ -48,6 +51,32 @@ async function subtractCoins(teamID,cash){
       //NOTE equipa não encontrada
     }
   }
+  return true
+}
+
+async function setCoins(teamID,cash){
+  if(teamID < 0 || cash<0){
+    return false;
+  }
+  let { data: Teams, error } = await supabase
+  .from('Teams')
+  .select('*').eq('IDTEAM', teamID)
+  if(Teams.length && error==null){
+    Teams[0].CASH=cash;
+    console.log(Teams[0].CASH)
+    const { updated, update_error } = await supabase
+    .from('Teams')
+    .update({ CASH: Teams[0].CASH })
+    .eq('IDTEAM', teamID)
+  }else{
+    //REVIEW gerar log file
+    if(error!=null){
+
+    }else{
+        //NOTE equipa não encontrada
+    }
+  }
+  return true
 }
 
 async function move_team(teamID){
@@ -88,9 +117,26 @@ async function move_team(teamID){
   }
 }
 
+async function increasePot(teamID,cash){
+  if(subtractCoins(teamID, cash))
+    addCoins(potID, cash)
+  //TODO se não tiver dinheiro
+}
+
+async function receivePot(teamID){
+  let { data: Teams, error } = await supabase
+  .from('Teams')
+  .select('*').eq('IDTEAM', potID)
+
+  if(Teams.length && error==null){
+    if(addCoins(teamID, Teams[0].CASH))
+      setCoins(potID, 0)
+    //TODO se não tiver dinheiro
+  }
+}
+
 /*addCoins(1,99);
 addCoins(12,99);
 addCoins(12,-99);
 subtractCoins(1,5);*/
-move_team(1);
 console.log(functions.logTime());
