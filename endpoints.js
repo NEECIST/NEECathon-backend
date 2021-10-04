863c52d4052250776a8d0cde1564f08e734a7d3a
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2021-10-04 10:24:19 +0100

    Updates to buyCart endpoint

--- endpoints.js
+++ endpoints.js
@@ -87,12 +87,15 @@ async function buyCart(teamID, cash, cart) {
     .from('Teams')
     .select("*").eq('IDTEAM', teamID)
   if(Teams.length && error === NULL) {
-    cart.forEach(component => {
-      let { data: Components, error } = await supabase
-        .from('Components')
-        .select("*").eq('IDCOMPONENT', component)
-      Teams[0].CASH -= Components[0].PRICE;
-      console.log(Components[0].PRICE, Teams[0].CASH);
+    let { data: Components, error } = await supabase
+      .from('Components')
+      .select("*").in('IDCOMPONENT', cart)
+    Components.forEach(component => {
+      if(--component.STOCK > 0) {
+        Teams[0].CASH -= component.PRICE
+      } else {
+        // TODO não há stock de componentes
+      }
     });
     if (Teams[0].CASH > 0) {
       const { updated, update_error } = await supabase
@@ -102,6 +105,9 @@ async function buyCart(teamID, cash, cart) {
     } else {
       // TODO equipa não tem dinheiro suficiente
     }
+    const { data, error } = await supabase
+      .from('Components')
+      .update({ STOCK: Components[0].STOCK }) // NOTE como dar update de um valor diferente para cada row ?
   } else {
     // REVIEW log file
     if (error != NULL) {
