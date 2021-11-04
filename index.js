4869acae3152a5a106355863547073553744d997
Midas <goncalomidoes@tecnico.ulisboa.pt>
2021-11-04 14:40:13 +0000

    -Shop routes created;

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 const app = express();
 import dotenv from "dotenv";
-import { mainRoutes} from "./routes/mainRoutes.js";
-
+import { mainRoutes } from "./routes/mainRoutes.js";
+import { shopRoutes } from "./routes/shopRoutes.js";
 dotenv.config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(mainRoutes);
+app.use(shopRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
---
+++ routes/shopRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+export const shopRoutes = express.Router();
+
+import * as endpoints from "../endpoints/shopEndpoints.js";
+
+shopRoutes.route("/products").get(function (req, res) {
+  endpoints.productsList().then(function (prodList) {
+    const converted = prodList.map(function (prodList) {
+      return { id: prodList.IDCOMPONENT, title: prodList.NAME, image: prodList.IMAGE, price: prodList.PRICE, stock: prodList.STOCK, description: prodList.REFSHEET, idHouse: prodList.IDHOUSE };
+    });
+    converted.sort(compareIds);
+    console.log(converted, "Routes");
+    res.json(converted);
+  });
+});
+
+function compareIds(a, b){
+    if ( a.id < b.id ){
+        return -1;
+      }
+      if ( a.id > b.id ){
+        return 1;
+      }
+      return 0;
+}
